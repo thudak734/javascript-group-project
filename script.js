547214a78880a6b46310d82cdb81880b4ef3f503
Kat Gilbert <katgilbert99@gmail.com>
2021-12-02 17:03:32 -0500

    making sure up to date

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ console.log(budget)
 let budgetInput = document.getElementById("budgetInput");
 budgetInput.innerText = budget;
 
-
 let fillerForBudgetDisplay = document.querySelector('h1'); //to display at top of page
-
 
 let category = document.getElementById("category");
 let amountLeftInBudget = document.getElementById("amountLeftInBudget"); //holds current budget
@@ -20,13 +18,13 @@ let billsTotal = 0;
 let currentBudget = 0;
 let totalSpent = 0;
 
-let submitBtn = document.querySelector("button");
+let submitBtn = document.getElementById("submitBtn");
 
 let amount = document.getElementById("amount");
 
 let userTotal = 0;// user weekly budget input
 
-submitBtn.addEventListener('submit', (event) => {
+submitBtn.addEventListener('submit', event => {
     event.preventDefault();
 
     if(category.value === "Entertainment"){
