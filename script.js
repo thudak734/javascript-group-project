150ef9a26dad63763c6a910cbfd5271fbcffde95
stovet <steven.topalian@gmail.com>
2021-12-02 15:14:30 -0500

    initial commit, assigning DOM variables

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let fillerForBudgetDisplay = document.querySelector('h1'); //to display at top of page
+
+
+let entertainment = document.getElementById("entertainment");
+let food = document.getElementById("food");
+let clothing = document.getElementById("clothing");
+let bills = document.getElementById("bills");
+let misc = document.getElementById("misc");
+let submitBtn = document.querySelector("button");
+
+let expenditure = document.getElementById("priceInput");
+
+
+submitBtn.addEventListener('click', () => {
+    
+
+
+});
