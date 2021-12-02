1d47b975c07a1e08f91debda11402e35769213bc
stovet <steven.topalian@gmail.com>
2021-12-02 16:32:56 -0500

    working on eventListener for entertainment

--- script.js
+++ script.js
@@ -13,18 +13,33 @@ console.log(budget)
 let fillerForBudgetDisplay = document.querySelector('h1'); //to display at top of page
 
 
-let entertainment = document.getElementById("entertainment");
-let food = document.getElementById("food");
-let clothing = document.getElementById("clothing");
-let bills = document.getElementById("bills");
-let misc = document.getElementById("misc");
+let category = document.getElementById("category");
+let amountLeftInBudget = document.getElementById("amountLeftInBudget"); //holds current budget
+let entertainmentTotalSpan = document.getElementById("entertainmentTotal");
+let totalSpentSpan = document.getElementById("totalSpent");
+let entertainmentTotal = 0;
+let foodTotal = 0;
+let clothingTotal = 0;
+let billsTotal = 0;
+let currentBudget = 0;
+let totalSpent = 0;
 
 let submitBtn = document.querySelector("button");
 
-let expenditure = document.getElementById("priceInput");
+let amount = document.getElementById("amount");
+
 let userTotal = 0;// user weekly budget input
 
-submitBtn.addEventListener('submit', () => {
+submitBtn.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if(category.value === "Entertainment"){
+        currentBudget = budget - amount.value;
+        entertainmentTotal += amount.value;
+        entertainmentTotalSpan.innerText = entertainmentTotal;
+        totalSpent += amount.value;
+        totalSpentSpan.innerText = totalSpent;
+    }
     
 
 
