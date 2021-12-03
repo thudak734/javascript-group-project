f23ae2ff25ddcc9f629b18174351eefb2303cfc0
stovet <steven.topalian@gmail.com>
2021-12-03 09:08:24 -0500

    It was saying I had a confilct so im committing

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
 let budget = parseInt(prompt("What is your weekly budget?"));
+
+
+let total = document.getElementById("total");
+
+
+total.innerText = `Total: $ ${budget}`;
+
 console.log(budget)
 
 let budgetInput = document.getElementById("budgetInput");
@@ -13,6 +20,8 @@ let category = document.getElementById("category");
 let amountLeftInBudget = document.getElementById("amountLeftInBudget"); //holds current budget
 let entertainmentTotalSpan = document.getElementById("entertainmentTotal");
 let totalSpentSpan = document.getElementById("totalSpent");
+let foodTotalSpan = document.getElementById("foodTotal");
+
 let entertainmentTotal = 0;
 let foodTotal = 0;
 let clothingTotal = 0;
@@ -33,6 +42,12 @@ submitBtn.addEventListener('submit', (event) => {
         currentBudget = budget - amount.value;
         entertainmentTotal += amount.value;
         entertainmentTotalSpan.innerText = entertainmentTotal;
+        totalSpent += amount.value;
+        totalSpentSpan.innerText = totalSpent;
+    } else if (category.value === "Food"){
+        currentBudget = budget - amount.value;
+        foodTotal += amount.value;
+        foodTotalSpan.innerText = foodTotal;
         totalSpent += amount.value;
         totalSpentSpan.innerText = totalSpent;
     }
