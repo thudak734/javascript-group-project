caeb13f6ceb15ae2acf03f7078c9f7ec2564467d
Kat Gilbert <katgilbert99@gmail.com>
2021-12-03 11:07:02 -0500

    list item addition working it's just ugly

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ mainForm.addEventListener('submit', event => {
     //// test code for new element being added
     newEntry = document.getElementById("newEntry");
     let listItem = document.createElement("li");
-    listItem.innertext = payee + " | Amount Spent: " + amount + " | Category: " + category;
+    listItem.innerText = payee.value + " | Amount: $" + amount.value + " | Category: " + category.value;
     newEntry.append(listItem);
 
     //// steve's code below
